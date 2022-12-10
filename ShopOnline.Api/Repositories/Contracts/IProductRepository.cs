f7e4184b2332813f6b91e1249cc794117e2dd39d
Talha Yazar <talhayazarg@gmail.com>
2022-12-10 23:32:46 +0300

    [Feat] : Created our first Contract as IProductRepository

---
+++ ShopOnline.Api/Repositories/Contracts/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using ShopOnline.Api.Entities;
+
+namespace ShopOnline.Api.Repositories.Contracts
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProduct(int id);
+        Task<IEnumerable<Product>> GetProducts();
+        Task<ProductCategory> GetProductCategory(int id);
+        Task<IEnumerable<ProductCategory>> GetProductCategories();
+    }
+}
