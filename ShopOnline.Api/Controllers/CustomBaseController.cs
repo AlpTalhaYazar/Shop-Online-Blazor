4e3ef250853d13a265ed5e0463f1ff24b5b683a8
Talha Yazar <talhayazarg@gmail.com>
2022-12-11 14:46:21 +0300

    [Feat] : Created our CustomBaseController as abstract class
    We will create our action results and inherit other basic implementations

---
+++ ShopOnline.Api/Controllers/CustomBaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public abstract class CustomBaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> customResponseDto)
+        {
+            var httpResponse = new ObjectResult(customResponseDto) { StatusCode = customResponseDto.StatusCode };
+
+            return httpResponse;
+        }
+    }
+}
