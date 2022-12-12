964e9abaf059dff7f46dcf2fc3e9eaf07f7b4978
Talha Yazar <talhayazarg@gmail.com>
2022-12-12 21:51:03 +0300

    [fix] : Better naming for entity property

--- ShopOnline.Api/Entities/CartItem.cs
+++ ShopOnline.Api/Entities/CartItem.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public int UserId { get; set; }
         public int ProductId { get; set; }
-        public int Qty { get; set; }
+        public int Quantity { get; set; }
     }
 }
--- ShopOnline.Api/Entities/Product.cs
+++ ShopOnline.Api/Entities/Product.cs
@@ -7,7 +7,7 @@
         public string Description { get; set; }
         public string ImageURL { get; set; }
         public decimal Price { get; set; }
-        public int Qty { get; set; }
+        public int Quantity { get; set; }
         public int CategoryId { get; set; }
     }
 }
