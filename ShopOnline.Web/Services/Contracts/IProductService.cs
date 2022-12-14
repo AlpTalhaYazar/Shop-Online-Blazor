f6d5876aefa3aa972a6919d7f70b8dfb22f53727
Talha Yazar <talhayazarg@gmail.com>
2022-12-14 23:59:25 +0300

    [feat] : Service and contract created for product

---
+++ ShopOnline.Web/Services/Contracts/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Web.Services.Contracts;
+
+public interface IProductService
+{
+    Task<ProductDto> GetProductByIdWithCategoryAsync(int id);
+    Task<IEnumerable<ProductDto>> GetProductsWithCategoryAsync();
+}
---
+++ ShopOnline.Web/Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+using System.Net.Http.Json;
+
+namespace ShopOnline.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public Task<ProductDto> GetProductByIdWithCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProductsWithCategoryAsync()
+        {
+            var response = await httpClient.GetFromJsonAsync<CustomResponseDto<IEnumerable<ProductDto>>>("api/v1/Product/GetProductsWithCategory");
+
+            var products = response.Data;
+
+            return products;
+        }
+    }
+}
