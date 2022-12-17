2976af13b18b8f6ecd2820364c647f2eeb56caab
Talha Yazar <talhayazarg@gmail.com>
2022-12-17 23:56:43 +0300

    [feat] : Product cards extracted
    Extracted into another component to build a better structure

---
+++ ShopOnline.Web/Pages/ProductPages/ProductCardBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Web.Pages.ProductPages;
+
+public class ProductCardBase : ComponentBase
+{
+    [Parameter]
+    public IEnumerable<ProductWithCategoryDto> ProductsWithCategory { get; set; }
+}
