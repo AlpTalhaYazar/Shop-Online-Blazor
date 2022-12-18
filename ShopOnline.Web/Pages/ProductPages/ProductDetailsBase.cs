aaf6a92c654f0456a5462406c54a96bb564e6f25
Talha Yazar <talhayazarg@gmail.com>
2022-12-18 13:53:10 +0300

    [feat] : Product details page and base class implemented
    Also add redirection from products page

---
+++ ShopOnline.Web/Pages/ProductPages/ProductDetailsBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Pages.ProductPages;
+
+public class ProductDetailsBase : ComponentBase
+{
+    [Parameter]
+    public int Id { get; set; }
+
+    [Inject]
+    public IProductService ProductService { get; set; }
+
+    public ProductWithCategoryDto ProductWithCategory { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        ProductWithCategory = await ProductService.GetProductByIdWithCategoryAsync(Id);
+    }
+}
