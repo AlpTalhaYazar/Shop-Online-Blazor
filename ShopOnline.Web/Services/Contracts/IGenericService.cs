1d48a0b5156976406638bff511a837e1e0e67f42
Alp Talha Yazar <talhayazarg@gmail.com>
2022-12-25 15:17:23 +0300

    [feat] : Generic service interface created

--- ShopOnline.Web/Services/Contracts/IGenericService.cs
+++ ShopOnline.Web/Services/Contracts/IGenericService.cs
@@ -1,6 +1,12 @@
-﻿namespace ShopOnline.Web.Services.Contracts;
+﻿using ShopOnline.Models.Dtos;
 
-public interface IGenericService
+namespace ShopOnline.Web.Services.Contracts;
+
+public interface IGenericService<T> where T : class
 {
-    
+    Task<CustomResponseDto<T>> GetByIdAsync(int id);
+    Task<CustomResponseDto<IEnumerable<T>>> GetAllAsync();
+    Task<CustomResponseDto<T>> AddAsync(T entity);
+    Task<CustomResponseDto<T>> UpdateAsync(T entity);
+    Task<CustomResponseDto<T>> DeleteAsync(T entity);
 }
